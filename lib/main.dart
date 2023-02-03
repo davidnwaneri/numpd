73551da12c62b726dab9443fc0fb2d7870fed810
David Nwaneri <davidn4333@gmail.com>
2023-02-03 16:37:46 +0100

    rename variables

--- lib/main.dart
+++ lib/main.dart
@@ -46,23 +46,31 @@ class _MyHomePageState extends State<MyHomePage> {
   int get cursorEndPosition => _controller.selection.extentOffset;
   bool get isTextSelected => cursorStartPosition != cursorEndPosition;
   bool get isCursorAtEnd => cursorStartPosition == _controller.text.length;
+  bool get isCursorBetweenText => !isTextSelected && !isCursorAtEnd;
 
   void inputNumber(int value) {
     if (isTextSelected) {
       _replaceTextSelection(value);
-    } else if (isCursorAtEnd) {
+      return;
+    }
+    if (isCursorAtEnd) {
       _inputNumberAtEnd(value);
-    } else {
+      return;
+    }
+    if (isCursorBetweenText) {
       _inputNumberBetweenText(value);
+      return;
     }
   }
 
   void _replaceTextSelection(int value) {
-    final text = _controller.text;
-    final firstPart = text.substring(0, cursorStartPosition);
-    final lastPart = text.substring(cursorEndPosition);
+    final currentNumbers = _controller.text;
+    final numbersBeforeSelection =
+        currentNumbers.substring(0, cursorStartPosition);
+    final numbersAfterSelection = currentNumbers.substring(cursorEndPosition);
     _controller.value = _controller.value.copyWith(
-      text: firstPart + value.toString() + lastPart,
+      // Set the new text, ignoring the selected text.
+      text: numbersBeforeSelection + value.toString() + numbersAfterSelection,
       selection: TextSelection.collapsed(
         offset: cursorStartPosition + 1,
       ),
@@ -70,11 +78,11 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void _inputNumberBetweenText(int value) {
-    final text = _controller.text;
-    final firstPart = text.substring(0, cursorStartPosition);
-    final lastPart = text.substring(cursorStartPosition);
+    final currentNumbers = _controller.text;
+    final numbersBeforeCursor = currentNumbers.substring(0, cursorStartPosition);
+    final numbersAfterCursor = currentNumbers.substring(cursorStartPosition);
     _controller.value = _controller.value.copyWith(
-      text: firstPart + value.toString() + lastPart,
+      text: numbersBeforeCursor + value.toString() + numbersAfterCursor,
       selection: TextSelection.collapsed(
         offset: cursorStartPosition + 1,
       ),
@@ -91,21 +99,80 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void clearLastInput() {
-    final value = _controller.text;
-    if (value.isEmpty) return;
+    if (_controller.text.isEmpty) return;
+    if (isTextSelected) {
+      _clearTextSelection();
+      return;
+    }
+    if (isCursorAtEnd) {
+      _clearNumberAtEnd();
+      return;
+    }
+    if (isCursorBetweenText) {
+      _clearNumberBetweenText();
+      return;
+    }
+  }
+
+  void _clearTextSelection() {
+    final currentNumbers = _controller.text;
+    final textBeforeSelection = currentNumbers.substring(0, cursorStartPosition);
+    final textAfterSelection = currentNumbers.substring(cursorEndPosition);
     _controller.value = _controller.value.copyWith(
-      text: value.substring(0, value.length - 1),
+      text: textBeforeSelection + textAfterSelection,
       selection: TextSelection.collapsed(
-        offset: _controller.text.length - 1,
+        offset: cursorStartPosition,
       ),
     );
   }
 
-  void clearAll() {
+  void _clearNumberBetweenText() {
+    final currentNumbers = _controller.text;
+    final numbersBeforeCursor =
+        currentNumbers.substring(0, cursorStartPosition);
+    final numbersAfterCursor = currentNumbers.substring(cursorStartPosition);
+    _controller.value = _controller.value.copyWith(
+      text: numbersBeforeCursor.substring(0, numbersBeforeCursor.length - 1) +
+          numbersAfterCursor,
+      selection: TextSelection.collapsed(
+        offset: cursorStartPosition - 1,
+      ),
+    );
+  }
+
+  void _clearNumberAtEnd() {
+    final currentNumbers = _controller.text;
+    _controller.value = _controller.value.copyWith(
+      text: currentNumbers.substring(0, currentNumbers.length - 1),
+      selection: TextSelection.collapsed(
+        offset: currentNumbers.length - 1,
+      ),
+    );
+  }
+
+  void clearAllInput() {
     if (_controller.text.isEmpty) return;
+    if (isCursorAtEnd) {
+      _clearAll();
+      return;
+    }
+    if (isCursorBetweenText) {
+      _clearAllBeforeCursor();
+      return;
+    }
+  }
+
+  void _clearAll() {
     _controller.value = _controller.value.copyWith(
       text: '',
       selection: const TextSelection.collapsed(offset: -1),
+    );
+  }
+
+  void _clearAllBeforeCursor() {
+    _controller.value = _controller.value.copyWith(
+      text: _controller.text.substring(cursorStartPosition),
+      selection: const TextSelection.collapsed(offset: 0),
     );
   }
 
@@ -135,7 +202,7 @@ class _MyHomePageState extends State<MyHomePage> {
             child: NumericPad(
               onInputNumber: inputNumber,
               onClearLastInput: clearLastInput,
-              onClearAll: clearAll,
+              onClearAll: clearAllInput,
             ),
           ),
         ],
