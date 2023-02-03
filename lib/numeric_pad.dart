ab42dd2f491fe850bcfd759422fa42511c55521d
David Nwaneri <davidn4333@gmail.com>
2023-02-03 16:54:54 +0100

    remove unnecessary widget

--- lib/numeric_pad.dart
+++ lib/numeric_pad.dart
@@ -16,58 +16,46 @@ class NumericPad extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Builder(builder: (context) {
-        // final state = NumericPadNotifier.of(context);
-        return ColoredBox(
-          color: Colors.white10,
-          child: Column(
-            children: [
-              for (int i = 1; i < 4; i++)
-                Expanded(
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      for (int j = 1; j < 4; j++)
-                        Expanded(
-                          child: KeyPad(
-                            number: (i - 1) * 3 + j,
-                            onKeyPress: () {
-                              // state.inputNumber((i - 1) * 3 + j);
-                              onInputNumber((i - 1) * 3 + j);
-                              // HapticFeedback.heavyImpact();
-                            },
-                          ),
-                        ),
-                    ],
-                  ),
-                ),
-              Expanded(
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    const Spacer(),
+    return ColoredBox(
+      color: Colors.white10,
+      child: Column(
+        children: [
+          for (int i = 1; i < 4; i++)
+            Expanded(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  for (int j = 1; j < 4; j++)
                     Expanded(
-                        child: KeyPad(
-                      number: 0,
-                      onKeyPress: () {
-                        onInputNumber(0);
-                        // HapticFeedback.heavyImpact();
-                      },
-                    )),
-                    Expanded(
-                      child: ClearButton(
-                        onClearLastInput: onClearLastInput,
-                        onClearAll: onClearAll,
+                      child: KeyPad(
+                        number: (i - 1) * 3 + j,
+                        onKeyPress: () => onInputNumber((i - 1) * 3 + j),
                       ),
                     ),
-                  ],
-                ),
+                ],
               ),
-            ],
+            ),
+          Expanded(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const Spacer(),
+                Expanded(
+                    child: KeyPad(
+                  number: 0,
+                  onKeyPress: () => onInputNumber(0),
+                )),
+                Expanded(
+                  child: ClearButton(
+                    onClearLastInput: onClearLastInput,
+                    onClearAll: onClearAll,
+                  ),
+                ),
+              ],
+            ),
           ),
-        );
-      }),
+        ],
+      ),
     );
   }
 }
